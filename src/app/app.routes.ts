9166d7d577df07eeda9ea08bf41bbcf4c11634ba
Antonio Lopes <antonio@media4all.com.br>
2025-03-15 13:13:08 -0300

    ✨feat: adiciona redirecionamento para a rota de produtos na configuração de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { CartPageComponent } from './pages/cart-page/cart-page.component';
 
 export const routes: Routes = [
     {
+        path: '',
+        redirectTo: '/products',
+        pathMatch: 'full'
+    },
+    {
         path: 'products',
         title: 'Produtos',
         component: ProductsComponent,
